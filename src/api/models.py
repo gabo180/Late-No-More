074a5809153fe01f70646232fe38d62c578f10c7
gabo180 <gabrieldejesushernandez@gmail.com>
2021-09-09 01:21:13 +0000

    JWT working on it

--- src/api/models.py
+++ src/api/models.py
@@ -3,16 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Profile(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), unique=True, nullable=False)
-    last_name = db.Column(db.String(120), unique=True, nullable=False)
-    username = db.Column(db.String(120), unique=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    last_name = db.Column(db.String(120), unique=False, nullable=False)
+    username = db.Column(db.String(120), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    employer_id = db.Column(db.ForeignKey('employer.id'), primary_key=True)
-    employee_id = db.Column(db.ForeignKey('employee.id'), primary_key=True)
-    messages_id = db.Column(db.ForeignKey('messages.id'), primary_key=True)
-    # user_id = db.Column(db.ForeignKey('user.id'), primary_key=True)
+    employer_id = db.Column(db.Integer, db.ForeignKey('employer.id'))
+    # employee_id = db.Column(db.Integer, db.ForeignKey('employee.id'))
+    messages_id = db.Column(db.Integer, db.ForeignKey('messages.id'))
+    employee_id = db.relationship("Employee", uselist=False, backref="profile")
 
     def __repr__(self):
         return '<Profile %r>' % self.username
@@ -30,8 +30,8 @@ class Profile(db.Model):
 
 class Messages(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    author_id = db.Column(db.Integer, unique=True, nullable=False)
-    recipient_id = db.Column(db.Integer, unique=True, nullable=False)
+    author_id = db.Column(db.Integer, unique=False, nullable=False)
+    recipient_id = db.Column(db.Integer, unique=False, nullable=False)
 
     def __repr__(self):
         return '<Messages %r>' % self.message
@@ -44,11 +44,15 @@ class Messages(db.Model):
 
 class Employee(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    role = db.Column(db.String(120), unique=True, nullable=False)
-    hourly_rate = db.Column(db.Float, unique=True, nullable=False)
-
+    role = db.Column(db.String(120), unique=False, nullable=False)
+    hourly_rate = db.Column(db.Float, unique=False, nullable=False)
+    profile_id = db.Column(db.Integer, db.ForeignKey('profile.id'))
+    
     def __repr__(self):
-        return '<Employee %r>' % self.employee
+        if self.profile is None:
+            return self.id
+        else:
+            return self.profile.name
 
     def serialize(self):
         return {
@@ -60,23 +64,26 @@ class Employee(db.Model):
 
 class Employer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    role = db.Column(db.String(120), unique=False, nullable=False)
+    hourly_rate = db.Column(db.Float, unique=False, nullable=False)
 
     def __repr__(self):
         return '<Employer %r>' % self.employer
 
     def serialize(self):
         return {
-            "id": self.id
+            "id": self.id,
+            "role": self.role,
+            "hourly_rate": self.hourly_rate
         }
 
 
 class Request(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    employer_id = db.Column(db.Integer, unique=True, nullable=False)
-    employee_id = db.Column(db.Integer, unique=True, nullable=False)
-    shift_id = db.Column(db.Integer, unique=True, nullable=False)
-    status = db.Column(db.String(50), unique=True, nullable=False)
-
+    employer_id = db.Column(db.Integer, unique=False, nullable=False)
+    employee_id = db.Column(db.Integer, unique=False, nullable=False)
+    shift_id = db.Column(db.Integer, unique=False, nullable=False)
+    status = db.Column(db.String(50), unique=False, nullable=False)
 
     def __repr__(self):
         return '<Request %r>' % self.request
@@ -89,10 +96,10 @@ class Request(db.Model):
 
 class Shift(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.String(40), unique=True, nullable=False)
-    hours = db.Column(db.String(50), unique=True, nullable=False)
-    role = db.Column(db.String(120), unique=True, nullable=False)
-    starting_time = db.Column(db.Integer, unique=True, nullable=False)
+    date = db.Column(db.String(40), unique=False, nullable=False)
+    hours = db.Column(db.String(50), unique=False, nullable=False)
+    role = db.Column(db.String(120), unique=False, nullable=False)
+    starting_time = db.Column(db.Integer, unique=False, nullable=False)
     ending_time = db.Column(db.Integer, unique=False, nullable=False)
 
     def __repr__(self):
@@ -112,9 +119,9 @@ class Shift(db.Model):
 
 class Punch(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    shift_id = db.Column(db.String(120), unique=True, nullable=False)
-    employee_id = db.Column(db.String(120), unique=True, nullable=False)
-    time_stamp = db.Column(db.Integer, unique=True, nullable=False)
+    shift_id = db.Column(db.String(120), unique=False, nullable=False)
+    employee_id = db.Column(db.String(120), unique=False, nullable=False)
+    time_stamp = db.Column(db.Integer, unique=False, nullable=False)
 
     def __repr__(self):
         return '<Punch %r>' % self.punch
