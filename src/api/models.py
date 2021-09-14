e9576ac050f38a35989fd778f7a7ceb2372f5442
marcoescmont <marcoescmont@gmail.com>
2021-09-14 01:43:44 +0000

    messages and request columns fixed on the DB

--- src/api/models.py
+++ src/api/models.py
@@ -10,13 +10,10 @@ class Profile(db.Model):
     username = db.Column(db.String(120), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    # employer_id = db.Column(db.Integer, db.ForeignKey('employer.id'))
-    # # employee_id = db.Column(db.Integer, db.ForeignKey('employee.id'))
-    # messages_id = db.Column(db.Integer, db.ForeignKey('messages.id'))
-    # employee_id = db.relationship("Employee", uselist=False, backref="profile")
     employee = db.relationship("Employee", backref="profile", uselist=False)
     employer = db.relationship("Employer", backref="profile", uselist=False)
-    messages = db.relationship('Messages', backref="profile", lazy=True)
+    messages_author = db.relationship('Messages_author', backref="author", lazy=True)
+    messages_recipient = db.relationship('Messages_recipient', backref="recipient", lazy=True)
     
 
     def __repr__(self):
@@ -30,26 +27,14 @@ class Profile(db.Model):
             "username": self.username,
             "email": self.email,
             "messages": list(map(lambda x: x.serialize(), self.messages))
-            # do not serialize the password, its a security breach
         }
-
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(50), nullable=False)
-#     addresses = db.relationship('Address', backref='person', lazy=True)
-
-# class Address(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     email = db.Column(db.String(120), nullable=False)
-#     person_id = db.Column(db.Integer, db.ForeignKey('person.id'),
-#         nullable=False)
 
 class Employee(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     role = db.Column(db.String(120), unique=False, nullable=False)
     hourly_rate = db.Column(db.Float, unique=False, nullable=False)
     profile_id = db.Column(db.Integer, db.ForeignKey('profile.id'))
-    # profile = db.relationship("Profile", back_populates="employee")
+    request_employee = db.relationship("Request", back_populates="employee")
     punch = db.relationship('Punch', backref="employee", lazy=True)
     
     def __repr__(self):
@@ -67,14 +52,26 @@ class Employee(db.Model):
         }
 
 
-class Messages(db.Model):
+class Messages_author(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    author_id = db.Column(db.Integer, unique=False, nullable=False)
-    recipient_id = db.Column(db.Integer, unique=False, nullable=False)
-    profile_id = db.Column(db.Integer, db.ForeignKey("profile.id"))
+    author_id = db.Column(db.Integer, db.ForeignKey('profile.id'))
+    
 
     def __repr__(self):
-        return '<Messages %r>' % self.id
+        return '<Messages_author %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id
+        }
+
+class Messages_recipient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    recipient_id = db.Column(db.Integer, db.ForeignKey('profile.id'))
+    
+
+    def __repr__(self):
+        return '<Messages_recipient %r>' % self.id
 
     def serialize(self):
         return {
@@ -86,6 +83,7 @@ class Employer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     role = db.Column(db.String(120), unique=False, nullable=False)
     hourly_rate = db.Column(db.Float, unique=False, nullable=False)
+    request_employer = db.relationship("Request", back_populates="employer")
     profile_id = db.Column(db.Integer, db.ForeignKey('profile.id'))
     
 
@@ -102,9 +100,12 @@ class Employer(db.Model):
 
 class Request(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    employer_id = db.Column(db.Integer, unique=False, nullable=False)
-    employee_id = db.Column(db.Integer, unique=False, nullable=False)
-    shift_id = db.Column(db.Integer, unique=False, nullable=False)
+    employer_id = db.Column(db.ForeignKey('employer.id'), primary_key=True)
+    employee_id = db.Column(db.ForeignKey('employee.id'), primary_key=True)
+    shift_id = db.Column(db.ForeignKey('shift.id'), primary_key=True)
+    employer = db.relationship("Employer", back_populates="request_employer")
+    employee = db.relationship("Employee", back_populates="request_employee")
+    shift = db.relationship("Shift", back_populates="request_shift")
     status = db.Column(db.String(50), unique=False, nullable=False)
 
     def __repr__(self):
@@ -118,12 +119,13 @@ class Request(db.Model):
 
 class Shift(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.String(40), unique=False, nullable=False)
-    hours = db.Column(db.String(50), unique=False, nullable=False)
+    date = db.Column(db.Date, unique=False, nullable=False)
+    hours = db.Column(db.Integer, unique=False, nullable=False)
     role = db.Column(db.String(120), unique=False, nullable=False)
-    starting_time = db.Column(db.Integer, unique=False, nullable=False)
-    ending_time = db.Column(db.Integer, unique=False, nullable=False)
+    starting_time = db.Column(db.DateTime, unique=False, nullable=False)
+    ending_time = db.Column(db.DateTime, unique=False, nullable=False)
     punch = db.relationship('Punch', backref="shift", lazy=True)
+    request_shift = db.relationship("Request", back_populates="shift")
 
     def __repr__(self):
         return '<Shift %r>' % self.id
@@ -136,13 +138,12 @@ class Shift(db.Model):
             "role": self.role,
             "starting_time": self.starting_time,
             "ending_time": self.ending_time
-            # do not serialize the password, its a security breach
         }
 
 
 class Punch(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    time_stamp = db.Column(db.Integer, unique=False, nullable=False)
+    time_stamp = db.Column(db.DateTime, unique=False, nullable=False)
     shift_id = db.Column(db.Integer, db.ForeignKey("shift.id"))
     employee_id = db.Column(db.Integer, db.ForeignKey("employee.id"))
     
